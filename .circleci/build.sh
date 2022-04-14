9dfd99051c324efbc9ef9f22a5d6762ded5c34d1
Mayur <flynryder427@gmail.com>
2022-04-14 12:50:41 +0530

    correct defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -49,7 +49,7 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 lavender-perf_defconfig
+    make O=out ARCH=arm64 lavender_defconfig
     make -j$(nproc --all) O=out \
                     ARCH=arm64 \
                     CC=clang \
